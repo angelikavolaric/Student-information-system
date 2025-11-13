89165c819ca2d6477fcc11c2414439b5a62e3bc8
angelikavolaric <volaric.angelika@gmail.com>
2025-11-13 09:47:53 +0100

    update classes for db

---
+++ angularApp/src/app/classes/address.ts
@@ -0,0 +1,6 @@
+export class Address {
+    homeAddress!: string;
+    postalCode!: string;
+    city!: string;
+    country!: string;
+}
--- angularApp/src/app/classes/student.ts
+++ angularApp/src/app/classes/student.ts
@@ -1,8 +1,14 @@
+import { Address } from "./address";
 import { Course } from "./course";
 
 export class Student {
     id!: string;
     name!: string;
     surname!: string;
+    gender!: string;
+    birthDate!: string;
+    email!: string | null;
+    phoneNumber!: string | null;
+    address!: Address;
     courses!: Array<Course>;
 }
