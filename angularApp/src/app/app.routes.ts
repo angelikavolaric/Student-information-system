923b1ae5470f7b889ee1070f87930178640c882b
angelikavolaric <volaric.angelika@gmail.com>
2025-11-13 10:04:02 +0100

    Lazy loading Student overview

--- angularApp/src/app/app.routes.ts
+++ angularApp/src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 import { AddStudentComponent } from './components/add-student/add-student.component';
 import { DeleteStudentComponent } from './components/delete-student/delete-student.component';
 import { EditStudentComponent } from './components/edit-student/edit-student.component';
-import { OverviewComponent } from './components/overview/overview.component';
 
 export const routes: Routes = [
     { path: 'add', component: AddStudentComponent },
     { path: 'delete/:id', component: DeleteStudentComponent },
     { path: 'edit/:id', component: EditStudentComponent },
-    { path: 'overview', component: OverviewComponent }
+    { path: 'overview', loadComponent: () => import('./components/overview/overview.component').then(m => m.OverviewComponent)}, //lazy loading component
+    { path: '**', redirectTo: 'overview'},
 ];
